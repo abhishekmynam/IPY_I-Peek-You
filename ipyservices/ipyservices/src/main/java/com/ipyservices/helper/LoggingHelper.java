93c0d2ea5cb81cea9d3e53048bed30060dcf86d1
abhishekmynam <abhishek.mynam@gmail.com>
2017-11-23 13:38:43 -0500

    Added config file parsing and xml reader

--- ipyservices/ipyservices/src/main/java/com/ipyservices/helper/LoggingHelper.java
+++ ipyservices/ipyservices/src/main/java/com/ipyservices/helper/LoggingHelper.java
@@ -8,6 +8,16 @@ import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
 import com.ipyservices.helper.entities.InfoLogger;
 import com.ipyservices.helper.interfaces.ILoggingHelper;
 
@@ -15,9 +25,8 @@ public class LoggingHelper implements ILoggingHelper {
 
 	public void LogInfo(InfoLogger info) {
 
-		LoggingConfig _logging = new LoggingConfig();
-		String fileName = _logging.LogfileName + DateTimeFormatter.ofPattern("yyyyMMdd").format(LocalDate.now()) + ".log";
-		String fileLocation = _logging.Location;
+		String fileLocation = LogfileLocation();
+		String fileName = "ipylog" + DateTimeFormatter.ofPattern("yyyyMMdd").format(LocalDate.now()) + ".log";
 		File logFile = new File(fileLocation + '/' + fileName);
 		try {
 			FileWriter writer = new FileWriter(logFile, true);
@@ -32,6 +41,37 @@ public class LoggingHelper implements ILoggingHelper {
 
 	}
 
+	private String LogfileLocation() {
+		DocumentBuilderFactory builder = DocumentBuilderFactory.newInstance();
+		try {
+			DocumentBuilder dBuilder = builder.newDocumentBuilder();
+			Document doc = dBuilder.parse(LoggingHelper.class.getResourceAsStream("config.xml"));
+			doc.normalize();
+			NodeList rootNodes = doc.getElementsByTagName("config");
+			Node rootNode = rootNodes.item(0);
+			Element rootElement = (Element) rootNode;
+			NodeList logNodes = rootElement.getElementsByTagName("logfile");
+			Node logNode = logNodes.item(0);
+			Element logElement = (Element) logNode;
+			Node location = logElement.getElementsByTagName("location").item(0);
+			Element loc = (Element) location;
+			return loc.getTextContent();
+			 
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+		
+	}
+
 	private String GenerateLogMessage(InfoLogger info) {
 		
 		String message = "";
@@ -39,7 +79,8 @@ public class LoggingHelper implements ILoggingHelper {
 		message += info.ClassName + "\n";
 		message += info.Method + "\n";
 		message += info.Message + "\n";
-		message += info.StackTrace;
+		message += info.StackTrace + "\n";
+		message += info.CustomMessage;
 		
 		return message;
 	}
